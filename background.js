c24489a3d28e85741a535209198fde5b9f8aa259
sylvieed <syisda@gmail.com>
2023-04-30 09:54:16 -0400

    Also save site when you go to preexisting tab

--- background.js
+++ background.js
@@ -1,12 +1,24 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (changeInfo.status === "complete") {
-    var url = tab.url;
-    var timestamp = new Date().getTime();
-    chrome.storage.local.get({websites: []}, function(result) {
-      var websites = result.websites;
-      websites.push({url: url, timestamp: timestamp});
-      chrome.storage.local.set({websites: websites});
-      chrome.runtime.sendMessage({action: "update_popup"});
-    });
+    saveSite(tab);
   }
 });
+
+chrome.tabs.onActivated.addListener(function(tabId) { 
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+      saveSite(tabs[0])
+  })
+});
+
+function saveSite(tab) {
+  console.log("Adding website to storage");
+  var url = tab.url;
+  console.log(url)
+  var timestamp = new Date().getTime();
+  chrome.storage.local.get({websites: []}, function(result) {
+    var websites = result.websites;
+    websites.push({url: url, timestamp: timestamp});
+    chrome.storage.local.set({websites: websites});
+    chrome.runtime.sendMessage({action: "update_popup"});
+  });
+}
